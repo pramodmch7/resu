c95d295f32c0935cee0ae8e099615f2ae4226722
unknown <you@example.com>
2020-12-07 01:04:13 +0530

    Updates

---
+++ precache-manifest.b4f760aa8252a9aedb607e27c7c7410d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6c7df194e063c075b64faf256c4ec8",
+    "url": "/resu/index.html"
+  },
+  {
+    "revision": "07ea37e740e2083591b7",
+    "url": "/resu/static/css/main.72b0fe3d.chunk.css"
+  },
+  {
+    "revision": "f1fee663de98c5f669f6",
+    "url": "/resu/static/js/2.4b33105f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resu/static/js/2.4b33105f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ea37e740e2083591b7",
+    "url": "/resu/static/js/main.6efa6ccf.chunk.js"
+  },
+  {
+    "revision": "255de2f0c18d0af16ebc",
+    "url": "/resu/static/js/runtime-main.f6dcbe45.js"
+  },
+  {
+    "revision": "4e39a3d074bc4769fef3a2ac9ae718d6",
+    "url": "/resu/static/media/C0.4e39a3d0.svg"
+  },
+  {
+    "revision": "b682590c22a517494dd2023b8bea889a",
+    "url": "/resu/static/media/C1.b682590c.svg"
+  }
+]);
